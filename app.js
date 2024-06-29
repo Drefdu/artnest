ca92d160ca152978c4c94c61c4317d23fe74fbc3
RizzFlow <136853145+RizzFlow@users.noreply.github.com>
2024-06-29 16:32:25 -0600

    Cambios
    Agregado de login, registro y separación de menú en .ejs

--- app.js
+++ app.js
@@ -14,6 +14,44 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+
+
+////////////////////////
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'formularios/login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'formularios/register.html'));
+});
+
+app.post('/login', (req, res) => {
+  // Simulate login validation
+  if (req.body.username === 'user' && req.body.password === 'password') {
+      req.session.user = req.body.username;
+      return res.redirect('/');
+  }
+  res.redirect('/login');
+});
+
+app.post('/register', (req, res) => {
+  // Handle user registration logic here
+  res.redirect('/login');
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user && req.path !== '/login' && req.path !== '/register') {
+      return res.redirect('/login');
+  }
+  next();
+});
+
+////////////////////////
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Aplicacion corriendo en http://localhost:${port}`);
 });
